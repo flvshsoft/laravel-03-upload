06855e01bf297fbb9a55924c0793f37e7703b72e
Yura <yuhaya.lissera@gmail.com>
2025-01-17 16:46:10 +0700

    fix: index

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/kontak', [App\Http\Controllers\HomeController::class, 'kontak'])->name('kontak');
 Route::get('/menu', [ App\Http\Controllers\MenuController::class, 'index'])->name('menu');
